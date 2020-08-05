123f595d28a7b5dbb8e89c2e1c54d9316378d719
Michael Chesterton <michael.chesterton@gmail.com>
2020-08-05 17:05:34 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # i3wm-workspace-switcher
-A productivity tool for i3wm to stop you temporarily from switching to certain workspaces
+A quickly written and barely tested productivity tool for i3wm to stop you temporarily from switching to certain workspaces
+
+
+In your i3 config where workspace switching is configured add "exec workspace.py" before the workspace keyword. eg:
+
+```
+bindsym $mod+Mod1+Left exec bin/workspace.py workspace prev         
+bindsym $mod+Mod1+Right exec bin/workspace.py workspace next        
+...                                                                 
+bindsym $mod+1 exec bin/workspace.py workspace $ws1                 
+bindsym $mod+2 exec bin/workspace.py workspace $ws2                 
+...       
+```
+
+Then to control which workspaces you allow yourself to switch to, echo them to `/tmp/wslock`
+
+`/tmp/wslock` is a text file with a whitespace separated list of workspace names you allow youself to switch to.
+If you try to switch to a workspace not allowed it will either skip over it if you use "next" or "prev", or it won't change to it if you try to switch directly to it.
+
+eg: `echo 3 5 work > /tmp/wslock`                                 
+will allow you to change to the workspaces named 3 5 and work     
+"next" and "prev" will cycle through in the specified direction and skip workspaces not in the allowed list.
+
+
+`> /tmp/wslock` will lock to current workspace
+`rm /tmp/wslock` will allow changing to any workspace
+
---
+++ workspace.py
@@ -0,0 +1,105 @@
+#!/bin/python3
+
+'''
+/tmp/wslock is a text file with a whitespace list of workspace names
+            that are permitted to change to
+            eg: `echo 3 4 5 work > /tmp/wslock`
+            will allow you to change to the workspaces named 3 4 5 and work
+            `> /tmp/wslock` will lock to current workspace
+            `rm /tmp/wslock` will allow changing to any workspace
+
+
+            i3 config
+            bindsym $mod+Mod1+Left exec bin/workspace.py workspace prev
+            bindsym $mod+Mod1+Right exec bin/workspace.py workspace next
+            ...
+            bindsym $mod+1 exec bin/workspace.py workspace $ws1
+            bindsym $mod+2 exec bin/workspace.py workspace $ws2
+            ...
+
+'''
+
+import json
+import sys
+import subprocess
+
+
+def new_workspace(focused, workspaces, args):
+    '''
+        get the index of the workspace we want to switch to
+    '''
+    if args[1] == 'prev':
+        new_ws = focused - 1
+        if new_ws < 0:
+            new_ws = len(workspaces) - 1
+    elif args[1] == 'next':
+        new_ws = focused + 1
+        if new_ws > len(workspaces) - 1:
+            new_ws = 0
+    else:
+        for i, workspace in enumerate(workspaces):
+            if workspace['name'] == args[1]:
+                new_ws = i
+                break
+    return new_ws
+
+
+def workspace(args):
+    # if /tmp/wslock doesn't exist or there are any errors reading it
+    # allow workspace changes
+    try:
+        wslockfd = open('/tmp/wslock')
+        wslock = wslockfd.read()
+        allowed_ws = wslock.split()
+    except Exception:
+        subprocess.run(["i3-msg", *args])
+        sys.exit(0)
+
+    # /tmp/wslock exists but it's empty, don't allow ws changes
+    if not len(allowed_ws):
+        sys.exit(0)
+
+    # read in the workspace data structure, any failures allow switching
+    try:
+        i3msg = subprocess.run(["i3-msg", "-t", "get_workspaces"],
+                               capture_output=True)
+        workspaces = json.loads(i3msg.stdout)
+    except Exception:
+        subprocess.run(["i3-msg", *args])
+        sys.exit(0)
+
+    # get the index of the workspaces list that has focus
+    focused = 0
+    for i, workspace in enumerate(workspaces):
+        if workspace['focused']:
+            focused = i
+            break
+
+    # get the index of the workspace we want to switch to
+    new_ws = new_workspace(focused, workspaces, args)
+
+    # check the new workspace is in the allowed list
+    if workspaces[new_ws]['name'] in allowed_ws:
+        subprocess.run(["i3-msg", "workspace", workspaces[new_ws]['name']])
+        sys.exit(0)
+
+    # if next or prev, cycle through the workspaces looking for the first match
+    # then switch to it
+    if args[1] == 'next' or args[1] == 'prev':
+        for _ in workspaces:
+            focused = new_ws
+            new_ws = new_workspace(focused, workspaces, args)
+            # check the new workspace is in the allowed list
+            if workspaces[new_ws]['name'] in allowed_ws:
+                subprocess.run(["i3-msg", "workspace",
+                                workspaces[new_ws]['name']])
+                sys.exit(0)
+
+    sys.exit(0)
+
+
+if len(sys.argv) == 1:
+    sys.exit(0)
+
+if sys.argv[1] == "workspace":
+    workspace(sys.argv[1:])
